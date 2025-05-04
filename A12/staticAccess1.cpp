8163d70082ab02ae14ce4fc75c30775ec41e7818
Benjamin Lampart <sera@icefish.dev>
2025-05-04 14:02:25 +0200

    A12/staticAccess1.cpp

---
+++ A12/staticAccess1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Number{
+    private:
+        static const int accessCount = 0;
+
+    public:
+        static int zero(){return 0;}
+        static int getCount() { return accessCount; }
+};
+	
+int main(){
+    Number::zero();
+    cout << Number::getCount() << endl;
+}
